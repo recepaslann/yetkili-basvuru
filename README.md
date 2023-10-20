b383b50ea2d20eeab5ae74991906a1ed19e139e1
recepaslann <recep.aslnn58@gmail.com>
2023-10-20 15:29:38 +0300

    yetkili basvuru
    discord gevele yetkili basvuru sistemi

---
+++ README.md
@@ -0,0 +1,5 @@
+# Altyapının Videosu
+https://youtu.be/UMBCF1cc7S8
+
+# Kurulum 
+`npm i discord.js`
---
+++ setting/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    token: "MTE2NDY0MDUyNzQwOTQ5NjE3NQ.G7vgld.RI4Db1A0sgy7jlMBYcymXVeDjvrnuqRSCS3Rr4", 
+    prefix: "/",
+    Footer: "GVL",
+    guildID: "1098000550932516989",
+    basvuruYt: "1120350908497272902",
+    yetkiRolleri: "1120349660893171883",
+    logKanalı: "1163898638532104294",
+    basvuruDurum: "1120350937060487259"
+}
+
+
